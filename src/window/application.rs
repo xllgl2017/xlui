7fc91499ab3d01b7e671f3c56c9a73ed09684521
xllgl2017 <xinlanxi2017@gmail.com>
2025-09-12 22:34:56 +0800

    fix create child window

--- src/window/application.rs
+++ src/window/application.rs
@@ -1,21 +1,20 @@
+use crate::error::UiResult;
 use crate::frame::App;
 use crate::map::Map;
 use crate::window::event::WindowEvent;
 use crate::window::ime::IME;
+#[cfg(target_os = "windows")]
+use crate::window::win32::Win32Window;
 use crate::window::wino::{EventLoopHandle, LoopWindow};
 #[cfg(target_os = "linux")]
 use crate::window::x11::ime::flag::Capabilities;
 #[cfg(target_os = "linux")]
 use crate::window::x11::X11Window;
 use crate::window::{WindowId, WindowType};
-use crate::WindowAttribute;
 use std::process::exit;
 use std::sync::Arc;
 #[cfg(target_os = "windows")]
 use windows::Win32::UI::WindowsAndMessaging::{SetWindowLongPtrW, GWLP_USERDATA};
-use crate::error::UiResult;
-#[cfg(target_os = "windows")]
-use crate::window::win32::Win32Window;
 
 pub struct Application {
     #[cfg(target_os = "linux")]
@@ -55,7 +54,7 @@ impl Application {
         })
     }
 
-    pub fn run(mut self)->UiResult<()> {
+    pub fn run(mut self) -> UiResult<()> {
         #[cfg(target_os = "windows")]
         unsafe { SetWindowLongPtrW(self.native_window.last_window().win32().hwnd, GWLP_USERDATA, &self as *const _ as isize); }
         loop {
@@ -67,8 +66,8 @@ impl Application {
             }
             if let Some(window) = self.loop_windows.get_mut(&wid) {
                 if let WindowEvent::CreateChild = event {
-                    let window_type = self.native_window.create_child_window(&window.app_ctx.context.window, &WindowAttribute::default())?;
                     let (app, attr) = window.app_ctx.context.new_window.take().unwrap();
+                    let window_type = self.native_window.create_child_window(&window.app_ctx.context.window, &attr)?;
                     let wid = window_type.id();
                     let loop_window = pollster::block_on(async { LoopWindow::create_window(app, window_type, &attr).await });
                     self.loop_windows.insert(wid, loop_window);
