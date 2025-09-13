fde11da424f0ba3e678e120f898fd58e510e2cec
xllgl2017 <xinlanxi2017@gmail.com>
2025-09-13 20:48:59 +0800

    changed `custom_font` example

--- examples/custom_font.rs
+++ examples/custom_font.rs
@@ -17,7 +17,7 @@ impl XlUiApp {
     fn new() -> XlUiApp {
         XlUiApp {
             count: 0,
-            status: "这里是Label".to_string(),
+            status: "使用`微软雅黑`字体".to_string(),
         }
     }
     fn add(&mut self, _: &mut Button, _: &mut Ui) {
@@ -35,24 +35,20 @@ impl XlUiApp {
 impl App for XlUiApp {
     fn draw(&mut self, ui: &mut Ui) {
         ui.window().request_update();
-        ui.add(Label::new("hello").with_id("status"));
-        ui.horizontal(|ui| {
-            ui.add(Button::new("+").width(30.0).height(30.0).connect(Self::add));
-            ui.add(Button::new("-").width(30.0).height(30.0).connect(Self::reduce));
-        });
+        ui.add(Label::new(self.status.as_str().color(Color::ORANGE)).with_id("status"));
+        for i in 10..=28 {
+            ui.add(Label::new(format!("当前字号: {}px", i).size(i as f32).color(Color::GREEN)));
+        }
     }
 
-    fn update(&mut self, ui: &mut Ui) {
-        let status: &mut Label = ui.get_widget("status").unwrap();
-        status.set_text(&self.status);
-    }
+    fn update(&mut self, _: &mut Ui) {}
 
 
     fn window_attributes(&self) -> WindowAttribute {
         WindowAttribute {
             font: Arc::new(Font::from_file("../xrs/target/res/font/微软雅黑.ttf")
                 .with_size(24.0)),
-            inner_size: (300, 200).into(),
+            inner_size: (300, 500).into(),
             ..Default::default()
         }
     }
